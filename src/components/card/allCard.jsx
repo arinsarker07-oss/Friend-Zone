89c3eaa0fe17c2e0af175e9531b0ba4d2840b7b0
arinsarker07-oss <arinsarker07@gmail.com>
2026-04-16 20:54:17 +0600

    card and card details and dynamic link part done

--- src/components/card/allCard.jsx
+++ src/components/card/allCard.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 
@@ -10,32 +11,35 @@ const statusColors = {
 };
 const AllCard = ({ friend }) => {
     return (
-        <div className="card bg-white shadow-xl border border-gray-100 p-8 flex flex-col items-center text-center transition-all hover:shadow-2xl">
-            <div className="avatar mb-6">
-                <div className="w-24 h-24 rounded-full ring ring-[#264d3e] ring-offset-base-100 ring-offset-2">
-                    <Image
-                        src={friend.picture}
-                        width={500}
-                        height={500}
-                        alt="Picture of the author"
-                    />
+        <Link href={`/${friend.id}`}>
+            <div className="card bg-white shadow-xl border border-gray-100 p-8 flex flex-col items-center text-center transition-all hover:shadow-2xl">
+                <div className="avatar mb-6">
+                    <div className="w-24 h-24 rounded-full ring ring-[#264d3e] ring-offset-base-100 ring-offset-2">
+                        <Image
+                            src={friend.picture}
+                            width={500}
+                            height={500}
+                            alt="Picture of the author"
+                        />
+                    </div>
+                </div>
+                <h2 className="text-xl font-extrabold text-[#1e293b]">{friend.name}</h2>
+                <p className="text-sm text-gray-400 font-medium mb-4">{friend.days_since_contact}d ago</p>
+                <div className="flex flex-col gap-3 items-center w-full mt-auto">
+                    <div className="flex flex-wrap justify-center gap-1">
+                        {friend.tags.map((tag, index) => (
+                            <span key={index} className="badge badge-success  badge-soft badge-sm uppercase text-[11px]   text-[#244D3F]">
+                                {tag}
+                            </span>
+                        ))}
+                    </div>
+                    <span className={`text-[10px] uppercase font-bold  mb-2 badge badge-sm ${statusColors[friend.status]}`}>
+                        {friend.status}
+                    </span>
                 </div>
             </div>
-            <h2 className="text-xl font-extrabold text-[#1e293b]">{friend.name}</h2>
-            <p className="text-sm text-gray-400 font-medium mb-4">{friend.days_since_contact}d ago</p>
-            <div className="flex flex-col gap-3 items-center w-full mt-auto">
-                <div className="flex flex-wrap justify-center gap-1">
-                    {friend.tags.map((tag, index) => (
-                        <span key={index} className="badge badge-success  badge-soft badge-sm uppercase text-[11px]   text-[#244D3F]">
-                            {tag}
-                        </span>
-                    ))}
-                </div>
-                <span className={`text-[10px] uppercase font-bold  mb-2 badge badge-sm ${statusColors[friend.status]}`}>
-                    {friend.status}
-                </span>
-            </div>
-        </div>
+        </Link>
+
     );
 };
 
